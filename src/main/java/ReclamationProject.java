270d401c2eecea3393627694061a246600cbeb4e
ammarahmed <ammarahmed@wirelessprv-10-193-222-26.near.illinois.edu>
2017-09-26 16:26:31 -0500

    Fixed Style

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,33 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * @author Ammar Ahmed
+ */
+public class ReclamationProject {
+    /**
+     *@param a a word
+     *@param b a word
+     *@return r
+     */
+    static String doit(final String a, final String b) {
+        String word1 = a;
+        String word2 = b;
+        if (a.length() > b.length()) {
+            word1 = b;
+            word2 = a;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = word1.length() - i; j > 0; j--) {
+                for (int k = 0; k < word2.length() - j; k++) {
+                    if (word1.regionMatches(i, word2, k, j) && j > r.length()) {
+                        r = word1.substring(i, i + j);
+                    }
+                 }
+            }
         } return r; }
 }
